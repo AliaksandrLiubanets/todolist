473dbae2dab65892aab7b0401946c686b62bde42
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-17 12:27:21 +0300

    Updates

---
+++ precache-manifest.a8484416753ac247ac2ab1e0e8e3bb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2170538ea85df673634cda1f0df54a2",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "06acdbbdf2ef3267086c",
+    "url": "/todolist/static/css/main.da3d83c9.chunk.css"
+  },
+  {
+    "revision": "dc8e4ac38a3556a7ca05",
+    "url": "/todolist/static/js/2.70b0d484.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.70b0d484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06acdbbdf2ef3267086c",
+    "url": "/todolist/static/js/main.4e9c0c74.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
