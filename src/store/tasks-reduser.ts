c57c4dca87f6b47ab2080bd990ae97660ca5f660
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-08 19:41:43 +0300

    Create test: correct task should be added to correct array in todolist-reducer.ts

--- src/store/tasks-reduser.ts
+++ src/store/tasks-reduser.ts
@@ -1,4 +1,6 @@
 import {FilterTaskType, TaskStateType} from '../App'
+import {TaskType} from '../Todolist'
+import {v1} from 'uuid'
 
 type RemoveTaskAT = {
     type: "REMOVE-TASK"
@@ -9,6 +11,7 @@ type RemoveTaskAT = {
 type AddTaskAT = {
     type: "ADD-TASK"
     title: string
+    todoListID: string
 }
 
 type ChangeTaskTitleAT = {
@@ -29,6 +32,14 @@ export const tasksReducer = (tasks: TaskStateType, action: ActionsType): TaskSta
     switch (action.type){
         case "REMOVE-TASK":
             return {...tasks, [action.todoListID]: tasks[action.todoListID].filter(task => task.id !== action.taskId)}
+        case 'ADD-TASK':
+            let newTask: TaskType
+            newTask = {
+                id: v1(),
+                title: action.title,
+                isDone: false
+            }
+            return {...tasks, [action.todoListID]: [newTask, ...tasks[action.todoListID]] }
 
         default:
             return tasks
@@ -37,7 +48,7 @@ export const tasksReducer = (tasks: TaskStateType, action: ActionsType): TaskSta
 
 export const removeTaskAC = (taskId: string, todoListID: string): RemoveTaskAT => ({type: "REMOVE-TASK", taskId, todoListID})
 
-// export const addTodolistAC = (title: string): AddTodolistAT => ({type: "ADD-TODOLIST", title})
+export const addTaskAC = (title: string, todoListID: string): AddTaskAT => ({type: "ADD-TASK", title, todoListID})
 //
 // export const changeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleAT => ({type: "CHANGE-TODOLIST-TITLE", id, title})
 //
