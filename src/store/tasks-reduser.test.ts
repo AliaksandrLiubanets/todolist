d3947a6bfd936d3d8532987bce59203ba05dc344
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-15 19:13:44 +0300

    refactor todolist test: add beforeEach()

--- src/store/tasks-reduser.test.ts
+++ src/store/tasks-reduser.test.ts
@@ -8,6 +8,21 @@ import {
 import {TaskStateType, TodolistType} from '../App'
 import {addTodolistAC, removeTodolistAC, todolistsReducer} from './todolist-reduser'
 
+beforeEach(() => {
+    const startState: TaskStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+})
+
 test('correct task should be deleted from correct array', () => {
     const startState: TaskStateType = {
         "todolistId1": [
