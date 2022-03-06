3614932f233a98e3294a456adeea04fe5ebf834b
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-03-06 20:30:08 +0300

    Refact: Task - Add TaskType

--- src/Task.tsx
+++ src/Task.tsx
@@ -4,8 +4,8 @@ import {Checkbox, IconButton, ListItem, ListItemIcon} from '@material-ui/core'
 import s from './Style.module.css'
 import EditableSpan from './EditableSpan'
 import DeleteIcon from '@material-ui/icons/Delete'
-import {TaskType} from './TodolistWithRedux'
 import {useDispatch} from 'react-redux'
+import {TaskType} from './api/todolist-api'
 
 type PropsType = {
     el: TaskType
@@ -25,12 +25,12 @@ export const Task = React.memo(({el, todolistId}: PropsType) => {
                      disableGutters
                      dense
                      style={{display: 'flex', justifyContent: 'space-between'}}
-                     className={el.isDone ? s.is_done : ''}>
+                     className={el.status === 2 ? s.is_done : ''}>
         <div style={{display: 'flex', alignItems: 'center', flex: 1}}>
             <ListItemIcon style={{display: 'block'}}>
                 <Checkbox onChange={onCheckboxCheckTask}
                           color={'primary'}
-                          checked={el.isDone}
+                          checked={el.status === 2}
                 />
             </ListItemIcon>
             <EditableSpan title={el.title}
