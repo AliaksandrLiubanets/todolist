a92f4f3b2b65f95b9256bc679f16757739ee2064
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-03-20 11:58:32 +0300

    Refactor app-reducer.ts - add error in initialState. Add app in combineReducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,7 +1,8 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-    status: 'loading' as RequestStatusType
+    status: 'idle' as RequestStatusType,
+    error: null as string | null
 }
 
 type InitialStateType = typeof initialState
@@ -10,13 +11,18 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
-export const setApptatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status})
+export const setAppStatusAC = (status: RequestStatusType) =>
+    ({type: 'APP/SET-STATUS', status} as const)
+export const setAppErrorAC = (error: null | string) => ({type: 'APP/SET-ERROR', error} as const)
 
-export type SetAppStatusAT = ReturnType<typeof setApptatusAC>
+export type SetAppStatusAT = ReturnType<typeof setAppStatusAC>
+export type SetAppErrorAT = ReturnType<typeof setAppErrorAC>
 
-type ActionsType = SetAppStatusAT
+type ActionsType = SetAppStatusAT | SetAppErrorAT
