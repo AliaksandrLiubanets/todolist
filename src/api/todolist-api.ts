bf0801d4e231576263b664d6d369f64f109285c3
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-05-01 15:56:11 +0300

    Change API-KEY in todolist-api.ts

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
-    headers: {'API-KEY': 'ec259ea8-b888-43af-83e9-f75c638bfe8f'}
+    headers: {'API-KEY': '7920275f-5e2b-4fa0-b4d6-1b5862086572'}
 })
 
 
